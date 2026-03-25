cf926745ba4725c9e24491db8ea9aac4f5c76dd6
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2026-03-25 14:55:42 +0100

    ref (customer) : remplacement de la variable totalAmount par l'appel d'une méthode getTotalCharge

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -21,7 +21,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + getName() + "\n";
 
@@ -31,14 +30,21 @@ public class Customer {
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    public double getTotalCharge() {
+        double totalAmount = 0;
+        for (Rental each : _rentals) {
+            totalAmount += each.getCharge();
+        }
+        return totalAmount;
     }
 
 }
